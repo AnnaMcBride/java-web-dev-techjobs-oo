70f13b69275de5ce4cc57c90ede9d64d6e402fda
Anna McBride <mcbrideanna28@gmail.com>
2020-12-20 20:26:17 -0600

    Step 4 Testing

---
+++ src/org/launchcode/techjobs_oo/Tests/JobTest.java
@@ -0,0 +1,46 @@
+package org.launchcode.techjobs_oo.Tests;
+
+
+//import org.testng.annotations.Test;
+import org.junit.Before;
+import static org.junit.Assert.*;
+import org.launchcode.techjobs_oo.*;
+import org.junit.Test;
+
+
+public class JobTest {
+
+    Job testJob1;
+    Job testJob2;
+    Job testJob3;
+    Job testJob4;
+    Job testJob5;
+
+   @Before
+   public void jobObject(){
+       testJob1 = new Job();
+       testJob2 = new Job();
+       testJob3 = new Job("Product tester", new Employer("ACME"), new Location("Desert"),
+                  new PositionType("Quality Control"), new CoreCompetency("Persistence"));
+       testJob4 = new Job("Product tester", new Employer("ACME"), new Location("Desert"),
+                  new PositionType("Quality Control"), new CoreCompetency("Persistence"));
+       testJob5 = new Job("Product tester", new Employer("ACME"), new Location("Desert"),
+                  new PositionType("Quality Control"), new CoreCompetency("Persistence"));
+   }
+
+    @Test
+    public void testSettingJobId(){
+       assertEquals(1, testJob2.getId() - testJob1.getId(), .001);
+        System.out.println(testJob1);
+    }
+
+    @Test
+    public void testJobConstructorSetsAllFields(){
+        assertEquals(true,testJob3 instanceof Job);
+    }
+
+    @Test
+    public void testJobsForEquality(){
+       assertEquals(false, testJob4.equals(testJob5));
+    }
+}
